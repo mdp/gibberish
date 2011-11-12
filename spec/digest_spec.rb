2d865c0c388cf50dd6d65b1743b3e4d1190c19f5
Glenn Rempe <glenn@rempe.us>
2011-11-12 13:59:53 -0800

    Add OpenSSL::Digest::SHA512 support.

--- spec/digest_spec.rb
+++ spec/digest_spec.rb
@@ -14,4 +14,8 @@ describe "A variety of digest methods" do
     Gibberish::SHA256("password").must_equal("5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8")
   end
 
+  it "should work with SHA512" do
+    Gibberish::SHA512("password").must_equal("b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86")
+  end
+
 end
