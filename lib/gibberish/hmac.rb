248d9ff92c9bc6c2d16d8c429da9540474e6db55
mdp <mdp@yahoo-inc.com>
2015-02-07 18:15:54 -0800

    Minor fix to HMAC docs [skip ci]

--- lib/gibberish/hmac.rb
+++ lib/gibberish/hmac.rb
@@ -1,19 +1,18 @@
 module Gibberish
-  # Easy to use HMAC, defaults to SHA256
+  # Easy to use HMAC
   #
   # ## Example
   #
-  #     Gibberish::HMAC('key', 'data')
+  #     Gibberish::HMAC256('key', 'data')
   #       #=> 5031fe3d989c6d1537a013fa6e739da23463fdaec3b70137d828e36ace221bd0
-  #     Gibberish::HMAC('key', 'data', :digest => :sha1) #=> 104152c5bfdca07bc633eebd46199f0255c9f49d
-  #     Gibberish::HMAC('key', 'data', :digest => :sha224)
+  #     Gibberish::HMAC1('key', 'data') # SHA1
+  #     #=> 104152c5bfdca07bc633eebd46199f0255c9f49d
+  #     Gibberish::HMAC224('key', 'data') # SHA224
   #       #=> 19424d4210e50d7a4521b5f0d54b4b0cff3060deddccfd894fda5b3b
-  #     Gibberish::HMAC('key', 'data', :digest => :sha256)
-  #       #=> 5031fe3d989c6d1537a013fa6e739da23463fdaec3b70137d828e36ace221bd0
-  #     Gibberish::HMAC('key', 'data', :digest => :sha384)
+  #     Gibberish::HMAC384('key', 'data') # SHA384
   #       #=> c5f97ad9fd1020c174d7dc02cf83c4c1bf15ee20ec555b690ad58e62da8a00ee
   #           44ccdb65cb8c80acfd127ebee568958a
-  #     Gibberish::HMAC('key', 'data', :digest => :sha512)
+  #     Gibberish::HMAC512('key', 'data') # SHA512
   #       #=> 3c5953a18f7303ec653ba170ae334fafa08e3846f2efe317b87efce82376253c
   #           b52a8c31ddcde5a3a2eee183c2b34cb91f85e64ddbc325f7692b199473579c58
   #
@@ -23,7 +22,7 @@ module Gibberish
   #
   # is the same as
   #
-  #     Gibberish::HMAC('password', 'stuff')
+  #     Gibberish::HMAC256('password', 'stuff')
   #
   class HMAC
     DIGEST = {
